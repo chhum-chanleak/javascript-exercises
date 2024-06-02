debe40604594135cd387ee7eda4c852ff03bfd2c
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-06-02 20:13:28 +0700

    feat: Impletment repeatString function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(word, count) {
 
+  if (count < 0) {
+    return 'ERROR';
+  }
+
+  let repeatedWords = "";
+
+  for(let i = 0; i < count; i += 1) {
+    repeatedWords += word;
+  }
+
+  return repeatedWords;
 };
 
-// Do not edit below this line
+// Do not edit below this linenod
 module.exports = repeatString;
