e26a7eee336b2480db386b97dedf7d07f24cd5a3
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-06-04 08:29:34 +0700

    test: Refactor and complete unit testing for isLeapYear function

--- 06_leapYears/leapYears.spec.js
+++ 06_leapYears/leapYears.spec.js
@@ -1,22 +1,23 @@
-const leapYears = require('./leapYears')
 
-describe('leapYears', () => {
+const isLeapYear = require('./leapYears');
+
+describe('isLeapYear', () => {
   test('works with non century years', () => {
-    expect(leapYears(1996)).toBe(true);
+    expect(isLeapYear(1996)).toBe(true);
   });
-  test.skip('works with non century years', () => {
-    expect(leapYears(1997)).toBe(false);
+  test('works with non century years', () => {
+    expect(isLeapYear(1997)).toBe(false);
   });
-  test.skip('works with ridiculously futuristic non century years', () => {
-    expect(leapYears(34992)).toBe(true);
+  test('works with ridiculously futuristic non century years', () => {
+    expect(isLeapYear(34992)).toBe(true);
   });
-  test.skip('works with century years', () => {
-    expect(leapYears(1900)).toBe(false);
+  test('works with century years', () => {
+    expect(isLeapYear(1900)).toBe(false);
   });
-  test.skip('works with century years', () => {
-    expect(leapYears(1600)).toBe(true);
+  test('works with century years', () => {
+    expect(isLeapYear(1600)).toBe(true);
   });
-  test.skip('works with century years', () => {
-    expect(leapYears(700)).toBe(false);
+  test('works with century years', () => {
+    expect(isLeapYear(700)).toBe(false);
   });
 });
