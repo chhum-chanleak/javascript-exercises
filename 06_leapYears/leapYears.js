7b2a59016c024cb0014758d43c095f3820851912
Chanleak Chhum <chhum.chanleak1994@gmail.com>
2024-06-04 08:30:11 +0700

    feat: Implement isLeapYear function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const isLeapYear = (year) => {
 
+  // Return 'false' when the inputted year is divisible by 4 or when the inputted year is both divisible by 100 and 400
+  if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+    return true;
+  } else if (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) {
+    return true;
+}
+
+  return false;
 };
 
 // Do not edit below this line
-module.exports = leapYears;
+module.exports = isLeapYear;
